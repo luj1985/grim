2d1711636002ccdbc1ca762c2f5f4f21a32f4a92
Lu, Jun <luj1985@gmail.com>
2016-05-14 00:23:58 +0800

    update registration workflow

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,24 @@
 class RegistrationsController < ApplicationController
+  
   def new
-
+    @registration = Registration.new
   end
+
+  def index
+    redirect_to "/"
+  end
+
+  def create
+    @registration = Registration.new(registration_params)
+    if @registration.save
+      render 'finished'
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def registration_params
+      params.require(:registration).permit(:company, :name, :telephone, :email, :duty)
+    end
 end
